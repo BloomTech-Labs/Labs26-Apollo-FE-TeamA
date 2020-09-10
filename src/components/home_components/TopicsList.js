865ee8bbc6270f8f82f52442f23591922ab5b348
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-10 11:35:37 -0600

    mapped the topics list

--- src/components/home_components/TopicsList.js
+++ src/components/home_components/TopicsList.js
@@ -1,6 +1,19 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
+import { TopicListContext } from "../../state/contexts/TopicListContext";
 
-const TopicsList = props =>{
+const TopicsList = props => {
+  const { topics } = useContext(TopicListContext);
 
-    
-}
+  return (
+    <>
+      {topics.map(item => (
+        <div>
+          <h1>
+            {item.topicName} {item.frequency}
+          </h1>
+          <h4>{item.updated_at}</h4>
+        </div>
+      ))}
+    </>
+  );
+};
