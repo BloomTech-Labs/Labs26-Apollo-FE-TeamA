d90017654ffe0766ed314e381988859b2e62524d
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-11 14:19:35 -0600

    getting the topic list to display

--- src/components/home_components/TopicsList.js
+++ src/components/home_components/TopicsList.js
@@ -1,12 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TopicListContext } from "../../state/contexts/TopicListContext";
+import axios from "axios";
 
 const TopicsList = props => {
   const { topics } = useContext(TopicListContext);
 
+  const [topiclist, settopiclist] = useState([]);
+
+  axios
+    .get("https://apollo-a-api.herokuapp.com/topic")
+    .then(res => {
+      console.log(res);
+      settopiclist(res.data);
+    })
+    .catch(err => console.log(err.response));
+
   return (
     <>
-      {topics.map(item => (
+      {topiclist.map(item => (
         <div>
           <h1>
             <p>{item.topicName}</p>
--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../../common';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../../common";
+import { TopicsList } from "../../home_components/TopicsList";
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
@@ -8,19 +9,7 @@ function RenderHomePage(props) {
     <div>
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
       <div>
-        <p>
-          This is an example of a common example of how we'd like for you to
-          approach components.
-        </p>
-        <p>
-          <Link to="/profile-list">Profiles Example</Link>
-        </p>
-        <p>
-          <Link to="/example-list">Example List of Items</Link>
-        </p>
-        <p>
-          <Link to="/datavis">Data Visualizations Example</Link>
-        </p>
+        <TopicsList />
         <p>
           <Button
             handleClick={() => authService.logout()}
