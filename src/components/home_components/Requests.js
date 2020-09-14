e779a41b1d5dbe7f36c3ff2fe7591a71466aeeff
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-14 11:30:41 -0600

    requests component made

--- src/components/home_components/Requests.js
+++ src/components/home_components/Requests.js
@@ -1,9 +1,21 @@
 import React, { useContext, useState } from "react";
 import { Requestscontext } from "../../state/contexts/RequestsContext";
+import axios from "axios";
 
 const Requests = props => {
   const { requestList } = useContext(Requestscontext);
   const [list, setlist] = useState([]);
 
-  return <></>;
+  axios.get("https://apollo-a-api.herokuapp.com/responses");
+
+  return (
+    <>
+      {list.map(item => (
+        <div>
+          <h1>{item.repondedby}</h1>
+          <h3>{item.created_at}</h3>
+        </div>
+      ))}
+    </>
+  );
 };
