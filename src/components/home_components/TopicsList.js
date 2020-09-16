dee4db693620050bebf65b2afe5279b7c16d8df8
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-16 16:51:08 -0600

    adjusted the topics.map to display the topics

--- src/components/home_components/TopicsList.js
+++ src/components/home_components/TopicsList.js
@@ -2,17 +2,19 @@ import React, { useContext, useState } from "react";
 import { TopicListContext } from "../../state/contexts/TopicListContext";
 import axios from "axios";
 
-const TopicsList = props => {
+const TopicsList = () => {
   const { topics } = useContext(TopicListContext);
 
   return (
     <>
       {topics.map(item => (
         <div>
-          <h1>{item.Name}</h1>
-          <h4>{item.updated_at}</h4>
+          <h3>{item.topicname}</h3>
+          <h6>Updated: {item.updated_at}</h6>
         </div>
       ))}
     </>
   );
 };
+
+export default TopicsList;
