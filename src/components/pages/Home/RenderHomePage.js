b08ff707b35c9d64a1ee13a4d0a07b2f6e2322c7
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-16 17:05:31 -0600

    Imported the context to display the data on the page

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../../common";
-import { TopicsList } from "../../home_components/TopicsList";
+import TopicsList from "../../home_components/TopicsList";
 import { Button } from "antd";
-import NewTopicContainer from "../NewTopic/NewTopicContainer";
+// import NewTopicContainer from "../NewTopic/NewTopicContainer";
+import { TopicListContext } from "../../../state/contexts/TopicListContext";
+import axios from "axios";
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
+  const [topics, setTopics] = useState([]);
+  const idToken = JSON.parse(localStorage.getItem("okta-token-storage")).idToken
+    .idToken;
+
+  axios
+    .get("https://apollo-a-api.herokuapp.com/topic", {
+      headers: { Authorization: `Bearer ${idToken}` }
+    })
+    .then(res => {
+      console.log(res.data);
+      setTopics(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
   return (
     <div>
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
       <div>
-        <TopicsList />
+        <TopicListContext.Provider value={{ topics }}>
+          <TopicsList />
+        </TopicListContext.Provider>
         <p>
           <Button
             handleClick={() => authService.logout()}
@@ -19,7 +38,6 @@ function RenderHomePage(props) {
           />
         </p>
       </div>
-
     </div>
   );
 }
