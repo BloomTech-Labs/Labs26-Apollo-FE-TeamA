8ecd72c8a587e49bc70ac6210ac1bc4126e412f9
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-16 21:20:25 -0600

    update RenderHomePage

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -2,15 +2,18 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import TopicsList from "../../home_components/TopicsList";
 import { Button } from "antd";
-// import NewTopicContainer from "../NewTopic/NewTopicContainer";
+import NewTopicContainer from "../NewTopic/NewTopicContainer";
 import { TopicListContext } from "../../../state/contexts/TopicListContext";
 import axios from "axios";
 
 function RenderHomePage(props) {
   const { userInfo, authService } = props;
+
   const [topics, setTopics] = useState([]);
   const idToken = JSON.parse(localStorage.getItem("okta-token-storage")).idToken
     .idToken;
+
+  console.log(userInfo);
 
   axios
     .get("https://apollo-a-api.herokuapp.com/topic", {
