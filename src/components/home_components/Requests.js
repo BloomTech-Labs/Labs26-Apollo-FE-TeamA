906ebd7c518b2908ac197795314bbe3ac7de616e
Hunter Stevens <hunterstevens1996@gmail.com>
2020-09-17 17:13:49 -0600

    update the Requests component

--- src/components/home_components/Requests.js
+++ src/components/home_components/Requests.js
@@ -1,16 +1,13 @@
 import React, { useContext, useState } from "react";
-import { Requestscontext } from "../../state/contexts/RequestsContext";
+import { RequestsContext } from "../../state/contexts/RequestsContext";
 import axios from "axios";
-
 const Requests = () => {
-  const { requestList } = useContext(Requestscontext);
-  const [list, setlist] = useState([]);
+  const { requestsList } = useContext(RequestsContext);
 
   return (
     <>
-      {requestList.map(item => (
+      {requestsList.map(item => (
         <div>
-          <h1>{item.repondedby}</h1>
           <h3>{item.created_at}</h3>
         </div>
       ))}
