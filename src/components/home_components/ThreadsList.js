cfe0778638a9d709590bdbe95b2aa0bb2184c9ec
Hunter Stevens <hunterstevens1996@gmail.com>
2020-10-06 14:18:55 -0600

    little update. Tried messing the files around but ultimately nothing new was added

--- src/components/home_components/ThreadsList.js
+++ src/components/home_components/ThreadsList.js
@@ -1,19 +1,33 @@
 import React, { useContext, useState } from "react";
 import { ThreadsContext } from "../../state/contexts/ThreadsContext";
 import axios from "axios";
+import { getToken } from "../../api/index";
 
 const ThreadsList = props => {
   const { threads } = useContext(ThreadsContext);
 
+  const getUserName = id => {
+    axios
+      .get(`https://apollo-a-api.herokuapp.com/profile/${id}`, getToken())
+      .then(res => {
+        console.log("GET /profile/:id", res);
+        return res.data.firstname;
+      })
+      .catch(err => console.log("getUserName", err));
+  };
+
   return (
     <>
-      {threads
-        ? threads.map(item => (
-            <div>
-              <p>{item.replies}</p>
-            </div>
-          ))
-        : null}
+      {threads ? (
+        threads.map(item => (
+          <div>
+            <h4>{getUserName(item.repliedby)}</h4>
+            <p>{item.replies}</p>
+          </div>
+        ))
+      ) : (
+        <p>There is no comments made for this Response</p>
+      )}
     </>
   );
 };
--- src/components/home_components/TopicsList.js
+++ src/components/home_components/TopicsList.js
@@ -19,6 +19,7 @@ const TopicsList = props => {
           onClick={() => {
             setTopicID(item.id);
             props.getSurveyList(item.id);
+            props.resetReqAndResID();
           }}
         >
           <h3>{item.topicname}</h3>
