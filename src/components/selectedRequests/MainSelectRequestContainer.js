ebba2e71a62686d31bc2e100bd9d820ebfb60f53
Hunter Stevens <hunterstevens1996@gmail.com>
2020-10-08 10:01:39 -0600

    deleted the selected request as the Request Component works in the MainTopic component

--- src/components/selectedRequests/MainSelectRequestContainer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RenderSelectRequest from "./RenderSelectRequest";
-
-function MainSelectRequestContainer(props) {
-  return <RenderSelectRequest getResponseList={props.getResponseList} />;
-}
-
-export default MainSelectRequestContainer;
